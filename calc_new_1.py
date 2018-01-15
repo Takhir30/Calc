dc9fd902e8515a812127bc764caf1f33142f0fe1
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-15 11:47:10 +0300

    stage 2 new try

--- calc_new_1.py
+++ calc_new_1.py
@@ -1,7 +1,14 @@
+import argparse
 import numexpr as ne
 
+
 def Main():
-    print(ne.evaluate('2+3+1-9'))
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-cl", "--calculate", help = "Expression to calculate", type = str)
+    args = parser.parse_args()
+    if args.calculate:
+        print(ne.evaluate(args.calculate))
 
 if __name__ == '__main__':
     Main()
