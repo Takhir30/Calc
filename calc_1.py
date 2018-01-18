cb8da0f8af82a9b50021f476767d7eacb7a2ef59
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-18 16:32:26 +0300

    new calc

--- calc_1.py
+++ calc_1.py
@@ -3,84 +3,34 @@ import math
 
 
 def calc(expr):
-    for i in ['sin', 'cos', '/', '*', '-', '+']:
-        if i in expr:
-            return action(i, expr)
-    return "%.2f" % float(expr)
-
-
-def left_num(sign, expr):
-    a = ''
-    a_index = expr.find(sign)
-    for i in expr[a_index-1::-1]:
-        if i.isdigit() or i == '.':
-            a += i
-            a_index -= 1
-        else:
-            break
-    return float(a[::-1]), a_index
-
-
-def right_num(sign, expr):
-    b = ''
-    b_index = expr.find(sign)
-    for i in expr[b_index + 1:]:
-        if i.isdigit() or i == '.':
-            b += i
-            b_index += 1
-        else:
-            break
-    return float(b), b_index + 1
-
-
-def action(sign, expr):
-    math_action = {'sin': trigonometry,
-                   'cos': trigonometry,
-                   '/': div,
-                   '*': mult,
-                   '-': sub,
-                   '+': add}
-    if sign in '/*-+':
-        a, a_index = left_num(sign, expr)
-        b, b_index = right_num(sign, expr)
-        new_expr = expr.replace(expr[a_index:b_index], math_action[sign](a, b))
-        return calc(new_expr)
-    else:
-        trigonometry(sign, expr)
-
-
-def trigonometry(tr_sign, expr):
-    b = ''
-    trig_index = expr.find(tr_sign) + 3
-    for i in expr[trig_index:]:
-        if i.isdigit() or i == '.':
-            b += i
-    trig = sign + b
-    rad_to_degree = int(b) * math.pi / 180
-    if tr_sign == 'sin':
-        new_expr = expr.replace(trig, str(math.sin(rad_to_degree)))
-    else:
-        new_expr = expr.replace(trig, str(math.cos(rad_to_degree)))
-    return calc(new_expr)
-
-
-def add(a, b):
-    return a + b
-
-
-def sub(a, b):
-    return a - b
-
-
-def mult(a, b):
-    return a * b
-
-
-def div(a, b):
     try:
-        return a / b
-    except ZeroDivisionError:
-        return("ZeroDivisionError")
+        num = float(expr)
+        return "%.2f" % float(expr)
+    except ValueError:
+        for symbol in ['+', '-', '*', '/']:
+            if symbol in expr:
+                symbol_index = expr.find(symbol)
+                left_expr = expr[:symbol_index]
+                right_expr = expr[symbol_index+1:]
+                return math_action(left_expr, right_expr, symbol)
+        if 'sin' in expr or 'cos' in expr:
+                return calc(trigonometry(expr))
+
+
+def math_action(left_expr, right_expr, symbol):
+    math_action = {'/': float.__truediv__,
+                   '*': float.__mul__,
+                   '-': float.__sub__,
+                   '+': float.__add__}
+    return math_action[symbol](calc(left_expr), calc(right_expr))
+
+
+def trigonometry(expr):
+    rad_to_degree = float(expr[3:]) * math.pi / 180
+    if expr[:3] == 'sin':
+        return math.sin(rad_to_degree)
+    else:
+        return math.cos(rad_to_degree)
 
 
 def fileoption():
