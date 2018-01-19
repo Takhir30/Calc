9c79e3fd7c6a46bdaca48e71908239ff99dfb50b
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-19 08:35:29 +0300

    fixed

--- test_file.py
+++ test_file.py
@@ -1,10 +1,14 @@
-import pytest,calc_1
+import pytest
+import calc_1
+
 
 def test_result():
     assert calc_1.calc('2*3/2*100') == 300.0
 
+
 def test_result_1():
     assert calc_1.calc('2*3/6-7') == -6.0
+
 
 def test_result_2():
     assert calc_1.calc('12/6-2*9/3') == -4.0
