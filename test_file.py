b9c98c12c24f7d715b19ebc4ea862ff38b3727b5
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-21 16:21:06 +0300

    some new tests

--- test_file.py
+++ test_file.py
@@ -1,14 +1,30 @@
 import pytest
-import calc_1
+import calc_rec
 
 
 def test_result():
-    assert calc_1.calc('2*3/2*100') == 300.0
+    assert calc_rec.calc('2*3/2*100') == 300.00
 
 
 def test_result_1():
-    assert calc_1.calc('2*3/6-7') == -6.0
+    assert calc_rec.calc('2*3/6-7') == -6.00
 
 
 def test_result_2():
-    assert calc_1.calc('12/6-2*9/3') == -4.0
+    assert calc_rec.calc('12/6-2*9/3') == -4.00
+
+
+def test_result_parenthesis():
+    assert calc_rec.calc('12/(6-2)*9/3') == 9.00
+
+
+def test_result_negative():
+    assert calc_rec.calc('(-1)/(-1)') == 1.00
+
+
+def test_result_trigonometry():
+    assert calc_rec.calc('12/(6-2)*9/3/sin30') == 18.00
+
+
+def test_result_wrong_symbol():
+    assert calc_rec.check('1+2+3gv') == 'Wrong input!!! You may use only "123456789.()+-*/" symbols! Try again!'
