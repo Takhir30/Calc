a95f039f2d240a99d5a06f00fc24f9001feab584
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-22 18:45:55 +0300

    one more calc

--- calc_1.py
+++ calc_1.py
@@ -15,26 +15,43 @@ def check(expr):
     for i in new_expr:
         if i not in symbols:
             return 'Wrong input!!! You may use only %s symbols! Try again!' % (symbols)
-    return calc(expr)
+    return str_to_list(expr)
+
+
+
+def str_to_list(expr_str):
+    if 'sin' in expr_str or 'cos' in expr_str:
+            return trigonometry(expr_str)
+    expr_list = ''
+    for i in expr_str:
+        if i.isdigit() or i == '.':
+            expr_list += i
+        elif i in '+-*/()' :
+            expr_list += ',' + i + ','
+    expr_list = expr_list.replace(',,',',').split(',')
+    expr_list = [x for x in expr_list if x]
+    return calc(expr_list)
 
 
 def calc(expr):
-    try:
-        return  float('%.2f' % float(expr))
-    except ValueError:
-        for symbol in ['(', '+-', '--', '*-', '/-', '+', '-', '*', '/']:
+    if expr[0] == '-':
+        expr[0:2] = ['-' + expr[1]]
+    if len(expr) == 1:
+        return  float('%.2f' % float(expr[0]))
+    else:
+        if '(' in expr:
+            return paren(expr)
+        for symbol in ['+', '-', '*', '/']:
             if symbol in expr:
-                if symbol == '(':
-                    return paren(expr)
-                else:
-                    symbol_index = expr.rfind(symbol)
-                    if symbol_index == 0:
-                        return calc('0' + expr)
-                    left_expr = expr[:symbol_index]
-                    right_expr = expr[symbol_index + 1:]
-                    return math_action(left_expr, right_expr, symbol[0])
-        if 'sin' in expr or 'cos' in expr:
-                return calc(trigonometry(expr))
+                symbol_index = -1
+                for i in expr[-1::-1]:
+                    if i == symbol:
+                        break
+                    symbol_index -= 1
+                left_expr = expr[:symbol_index]
+                right_expr = expr[symbol_index + 1:]
+                return math_action(left_expr, right_expr, symbol)
+
 
 
 def paren(expr):        #parenthesis
@@ -46,8 +63,8 @@ def paren(expr):        #parenthesis
             right_index = i_index
             break
         i_index += 1
-    new_expr = expr.replace(expr[left_index:right_index + 1], str(calc(expr[left_index + 1:right_index])))
-    return calc(new_expr)
+    expr[left_index:right_index + 1] = [calc(expr[left_index + 1:right_index])]
+    return calc(expr)
 
 
 def math_action(left_expr, right_expr, symbol):
@@ -59,17 +76,28 @@ def math_action(left_expr, right_expr, symbol):
 
 
 def trigonometry(expr):
-    rad_to_degree = float(expr[3:]) * math.pi / 180
-    if expr[:3] == 'sin':
-        return math.sin(rad_to_degree)
-    else:
-        return math.cos(rad_to_degree)
+    rad = ''
+    for i in ['sin', 'cos']:
+        if i in expr:
+            tr_l_index = expr.find(i)
+            for j in expr[tr_l_index + 3:]:
+                if j.isdigit() or j == '.':
+                    rad += j
+                else:
+                    break
+        rad_to_degree = float(rad) * math.pi / 180
+        tr_r_index = tr_l_index + 3 + len(rad)
+        if i == 'sin':
+            expr = expr.replace(expr[tr_l_index:tr_r_index], str(math.sin(rad_to_degree)))
+        else:
+            expr = expr.replace(expr[tr_l_index:tr_r_index], str(math.cos(rad_to_degree)))
+        return str_to_list(expr)
 
 
 def fileoption():
     with open('text.txt') as line:
         num = line.readline().strip()
-        b = check(num)
+        b = calc(num)
     with open(input("Type here the name of the file:  "), 'a+') as new_line:
         new_line.write(num + '\t')
         new_line.write(b)
@@ -83,12 +111,12 @@ def main():
     parser.add_argument("-kb", "--keyboard", type = str)
     args = parser.parse_args()
     if args.keyboard:
-        print(check(args.keyboard))
+        print(calc(args.keyboard))
     elif args.fileoption:
         fileoption()
         print("New file!!")
     else:
-        print(check('1+1'))
+        print(calc('1+1'))
 
 
 if __name__ == '__main__':
