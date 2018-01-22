556db7bc48e6e923621a4a3a846001203ffd65fc
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-22 10:09:21 +0300

    fixed

--- test_file.py
+++ test_file.py
@@ -1,30 +1,30 @@
 import pytest
-import calc_rec
+import calc_1
 
 
 def test_result():
-    assert calc_rec.calc('2*3/2*100') == 300.00
+    assert calc_1.calc('2*3/2*100') == 300.00
 
 
 def test_result_1():
-    assert calc_rec.calc('2*3/6-7') == -6.00
+    assert calc_1.calc('2*3/6-7') == -6.00
 
 
 def test_result_2():
-    assert calc_rec.calc('12/6-2*9/3') == -4.00
+    assert calc_1.calc('12/6-2*9/3') == -4.00
 
 
 def test_result_parenthesis():
-    assert calc_rec.calc('12/(6-2)*9/3') == 9.00
+    assert calc_1.calc('12/(6-2)*9/3') == 9.00
 
 
 def test_result_negative():
-    assert calc_rec.calc('(-1)/(-1)') == 1.00
+    assert calc_1.calc('(-1)/(-1)') == 1.00
 
 
 def test_result_trigonometry():
-    assert calc_rec.calc('12/(6-2)*9/3/sin30') == 18.00
+    assert calc_1.calc('12/(6-2)*9/3/sin30') == 18.00
 
 
 def test_result_wrong_symbol():
-    assert calc_rec.check('1+2+3gv') == 'Wrong input!!! You may use only "123456789.()+-*/" symbols! Try again!'
+    assert calc_1.check('1+2+3gv') == 'Wrong input!!! You may use only 1234567890.()+-*/ symbols! Try again!'
