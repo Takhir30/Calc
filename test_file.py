f7a43935d4c541f23b0fba26b5bc0f9117dd1b0e
Takhir30 <t.baiheryiev@andersenlab.com>
2018-01-22 18:48:35 +0300

    new test to new calc

--- test_file.py
+++ test_file.py
@@ -3,27 +3,27 @@ import calc_1
 
 
 def test_result():
-    assert calc_1.calc('2*3/2*100') == 300.00
+    assert calc_1.check('2*3/2*100') == 300.00
 
 
 def test_result_1():
-    assert calc_1.calc('2*3/6-7') == -6.00
+    assert calc_1.check('2*3/6-7') == -6.00
 
 
 def test_result_2():
-    assert calc_1.calc('12/6-2*9/3') == -4.00
+    assert calc_1.check('12/6-2*9/3') == -4.00
 
 
 def test_result_parenthesis():
-    assert calc_1.calc('12/(6-2)*9/3') == 9.00
+    assert calc_1.check('12/(6-2)*9/3') == 9.00
 
 
 def test_result_negative():
-    assert calc_1.calc('(-1)/(-1)') == 1.00
+    assert calc_1.check('(-1)/(-1)') == 1.00
 
 
 def test_result_trigonometry():
-    assert calc_1.calc('12/(6-2)*9/3/sin30') == 18.00
+    assert calc_1.check('12/(6-2)*9/3/sin30') == 18.00
 
 
 def test_result_wrong_symbol():
